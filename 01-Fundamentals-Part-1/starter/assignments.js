041ef1e7b2481491866fed4f33caffe1eea586ee
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-06-10 14:04:35 -0400

    Data Types and initializing variables
    Learning about data types and initializing variables.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,19 @@
-let country='Canada';
-let continent='North America';
+
+const country='Canada';
+const continent='North America';
 let population=38000000;
 
 console.log(continent);
 console.log(population);
+
+
+const isIsland=false;
+let language = 'English';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);   
+
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -5,5 +6,47 @@ console.log('Jonas');
 
 console.log('23');
 
+
 let firstName = 'Matilda';
 console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 23);
+console.log(typeof 'Jonas');    
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year=1998;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+let age=30;
+age=31;
+
+const birthYear=1998;
+
+var job='programmer';
+job='teacher';
