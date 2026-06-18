c747a5cacbcc5d18c25385bd0e2d4775e0bc9e3d
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-06-18 14:04:22 -0400

    functions
    Introduction to functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,46 @@
+
 'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+*/
+
+/*
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+};
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+function calcAge1(birthYear){
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function(birthYear){
+
+    return 2037 -birthYear;
+}
+
+
+const age2 =calcAge2(1991);
+console.log(age1, age2);
