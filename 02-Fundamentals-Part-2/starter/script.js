87d79456551655e12b6018ee8666fb627bf1ff6a
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-07-10 03:40:51 -0400

    loops update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -218,6 +218,7 @@ console.log(jonas)
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 */
 
+/*
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
@@ -243,4 +244,62 @@ console.log(jonas.calcAge());
 
  console.log(jonas.getSummary());
  console.log('Jonas has ' + jonas.calcAge() + ' years old and he has ' + (jonas.hasDriversLicense? 'a' : 'no') + ' drivers license');
- 
+*/
+
+// Loops
+
+
+/*
+for(let rep = 1; rep <= 10; rep++){
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+}
+*/
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+
+  } 
+
+
+  const types = [];
+
+  for(let i=0; i< jonas.length; i++){
+    console.log(jonas[i], typeof jonas[i]);
+
+    types.push(typeof jonas[i]);
+
+  }
+
+  console.log(types);
+
+
+
+  constyears = [1991, 2007, 1969, 2020];
+  const ages = [];
+
+  for(let i=0; i< years.length; i++){
+    ages.push(2037 - years[i]);
+  }
+  console.log(ages);
+
+
+
+  // continue and break
+
+  for(let i=0; i< jonas.length; i++){
+    if (typeof jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+
+
+  }
+
+   for(let i=0; i< jonas.length; i++){
+    if (typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
+
+
+  }
