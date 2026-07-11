38dcacb0e034e0799e6ea032e7618e2c3d63e1f7
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-07-11 05:00:21 -0400

    Update loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -255,6 +255,8 @@ for(let rep = 1; rep <= 10; rep++){
 }
 */
 
+
+/*
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
@@ -303,3 +305,47 @@ const jonas = {
 
 
   }
+*/
+
+
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+   2037 - 1991, 
+  'teacher', 
+  ['Michael', 'Peter', 'Steven']
+];
+
+
+for (let i = jonas.length - 1; i >= 0; i--){
+  console.log(i, jonas[i]);
+}
+
+
+for (let exercise = 1; exercise < 4; exercise++){
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++){
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♂️`);
+  }
+}
+
+*/
+
+for (let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weight repetition ${rep} 🏋️‍♂️`);
+  }
+
+  let rep = 1;
+  while(rep <= 10){
+    console.log(`WHILE: Lifting weight repetition ${rep} 🏋️‍♂️`);
+    rep++;
+  }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while(dice !== 6){
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
