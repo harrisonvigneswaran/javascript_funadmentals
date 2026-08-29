1e7f66b5fb0b98a6318cece7de75a976881e45d4
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-08-29 14:04:17 -0400

    update

--- chapter-three/starter/script.js
+++ chapter-three/starter/script.js
@@ -120,3 +120,37 @@ function multiply(a,b){
 
 }
 console.log(multiply(3,3))
+
+//ffunctions that will return cows and chickens
+/*
+function printFarmInventory(cows, chickens) {
+let cowString = String(cows);
+while (cowString.length < 3) {
+cowString = "0" + cowString;
+}
+console.log(`${cowString} Cows`);
+let chickenString = String(chickens);
+while (chickenString.length < 3) {
+chickenString = "0" + chickenString;
+}
+console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11);
+
+*/
+
+//Add the pigs so we have to refactor functions
+
+function zeroPad(number, width) {
+let string = String(number);
+while (string.length < width) {
+string = "0" + string;
+}
+return string;
+}
+function printFarmInventory(cows, chickens, pigs) {
+console.log(`${zeroPad(cows, 3)} Cows`);
+console.log(`${zeroPad(chickens, 3)} Chickens`);
+console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
+printFarmInventory(7, 16, 3);
