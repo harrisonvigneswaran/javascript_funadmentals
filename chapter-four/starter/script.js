0426733084b87afa696d37e5f8be105f01246540
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-09-17 14:06:09 -0400

    Update objects

---
+++ chapter-four/starter/script.js
@@ -0,0 +1,21 @@
+let listOfNumbers = [2, 3, 5, 7,11];
+
+console.log(listOfNumbers);
+
+// Methods
+
+let sequence = [1,2,3]
+
+sequence.push(4); // adds 4 to the end of the array
+console.log(sequence);  
+
+//Objects
+
+let day1 ={
+    squirrel:false,
+    events:["work", "touched tree", "pizza", "running"]     
+
+}
+
+day1.wolf = false; // adds a new property to the object
+console.log(day1);
