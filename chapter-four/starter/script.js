1e16b8f0b56f959ba1301fbd2eea73700218e90a
harrisonvigneswaranDc <vigneswaranharrison@gmail.com>
2026-09-19 14:04:09 -0400

    objects

--- chapter-four/starter/script.js
+++ chapter-four/starter/script.js
@@ -19,3 +19,30 @@ let day1 ={
 
 day1.wolf = false; // adds a new property to the object
 console.log(day1);
+
+
+let descriptions = {
+    work: "Went to work",
+    touchedTree: "Touched a tree",
+    pizza: "Ate pizza",
+    running: "Went for a run"
+};
+
+console.log("work" in descriptions); // checks if the property exists in the object
+
+console.log(Object.keys(descriptions)); // gets an array of all property names in the object
+
+console.log(Object.keys({x: 0, y: 0, z: 2}));// gets an array of all property names in the object
+
+Object.assign(descriptions, {exercise: "Went for a walk"}); // adds a new property to the object
+console.log(descriptions);
+
+// Arrays with objects
+
+let journal = [
+    {events: ["work", "touched tree", "pizza", "running"], squirrel: false},
+    {events: ["work", "ice cream", "cauliflower", "lasagna"], squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts"], squirrel: true}
+];
+
+console.log(journal[1].events); // accesses the events property of the second object in the array
